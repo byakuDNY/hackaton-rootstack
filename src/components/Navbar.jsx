bcf500474f17c3ea3abce8b2172b4cb13e2b155a
Sayo-420 <avilescarlos435@gmail.com>
2024-10-21 14:10:05 -0500

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,24 @@
-const Navbar = () => {
-  return <div>Navbar</div>;
+import React from "react";
+
+const navbar = () => {
+
+
+  return (
+    <nav className="backgroundColor: to-black text-white flex justify-between">
+    
+      <div className="nav-logo">
+        <a href="/">Logo</a>
+      </div>
+
+      <div className="flex flex-row flex-nowrap items-center">
+        <a href="/">Inicio</a>
+        <a href="/about">Sobre Nosotros</a>
+        <a href="/contact">Contacto</a>
+      </div>
+    
+    </nav>
+  );
+
 };
 
-export default Navbar;
+export default navbar;
