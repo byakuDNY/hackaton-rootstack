9d6ad1a7b6f95bbe2362b70bccb0d13d015bef6f
Roberto Luo <r13luo@gmail.com>
2024-10-21 12:43:22 -0500

    ee

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Product from "./pages/product/Product";
-import Home from "./pages/home/Home"
+import Home from "./pages/home/Home";
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home/>}></Route>
+        <Route path="/" element={<Home />}></Route>
         <Route path="/product" element={<Product />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/signup" element={<Signup />}></Route>
       </Routes>
     </Router>
   );
