93884bb58b58b6434185198626e9150f66c31e2e
Sayo-420 <avilescarlos435@gmail.com>
2024-10-21 17:22:01 -0500

    Colores de perfiles

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -7,41 +7,41 @@ const About = () => {
       <div><Navbar /></div>
       
       <div className='text-black flex justify-evenly my-10'>
-        <div>
-          <img src="" alt="Foto de perfil" />
-          <h1 className='text-2xl'>Roberto Luo</h1>
-          <p className='text-lg'>
+        <div className='border-4 border-lime-600 rounded-xl flex items-center flex-col bg-lime-400'>
+          <img className='m-2' src="" alt="Foto de perfil" />
+          <h1 className='text-2xl m-2'>Roberto Luo</h1>
+          <div><p className='text-lg m-2'>
             Edad: 21<br />
             Numero telefonico: 6520-0235<br />
             Correo electronico: <br />
             Cualidades: <br />
             Conocimientos: <br />
 
-          </p>
+          </p></div>
         </div>
-        <div>
-          <img src="" alt="Foto de perfil" />
-          <h1 className='text-2xl'>Enrique Wen</h1>
-          <p className='text-lg'>
+        <div className='border-4 border-lime-600 rounded-xl flex items-center flex-col bg-lime-400'>
+          <img className='m-2' src="" alt="Foto de perfil" />
+          <h1 className='text-2xl m-2'>Enrique Wen</h1>
+          <div><p className='text-lg m-2'>
             Edad: 21<br />
             Numero telefonico: 6740-5585<br />
             Correo electronico: <br />
             Cualidades: <br />
             Conocimientos: <br />
 
-          </p>
+          </p></div>
         </div>
-        <div>
-          <img src="" alt="Foto de perfil" />
-          <h1 className='text-2xl'>Carlos Avilés</h1>
-          <p className='text-lg'>
+        <div className='border-4 border-lime-600 rounded-xl flex items-center flex-col bg-lime-400'>
+          <img className='m-2' src="" alt="Foto de perfil" />
+          <h1 className='text-2xl m-2'>Carlos Avilés</h1>
+          <div><p className='text-lg m-2'>
             Edad: 20<br />
             Numero telefonico: 6549-8261<br />
             Correo electronico: carlosavilesia@gmal.com<br />
             Cualidades: <br />
             Conocimientos: <br />
 
-          </p>
+          </p></div>
         </div>
       </div>
 
