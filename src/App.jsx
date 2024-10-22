fcc5c5588354264c381a14a796b7e32f529508ac
Roberto Luo <r13luo.gmail.com>
2024-10-21 23:59:47 -0500

    bug

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Product from "./pages/Product";
 import Home from "./pages/Home";
 import Signup from "./pages/Signup";
-import Login from "./pages/Login";
+import Login from "./pages/login";
 import { Toaster } from "react-hot-toast";
 import Catalogo from "./pages/Catalogo";
 import About from "./pages/About";
